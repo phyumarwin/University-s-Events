8ce82eeb15384468efb756f6e4ab40171b63624a
unknown <phyumarwin99309@gmail.com>
2024-07-05 14:13:21 +0630

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My University's Events</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+     crossorigin="anonymous">
+</head>
+<body>
+    <nav>
+        <label class="logo">Computer University (UCSMTLA)</label>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="" class="btn btn-success btn-sm">Login</a></li>
+        </ul>
+    </nav>
+
+    <div class="section1">
+        <label class="img_text">My University's Events</label>
+        <img class="main_img" src="images/11u.webp" alt="">
+    </div>
+
+</body>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
+crossorigin="anonymous">
+</script>
+</html>
